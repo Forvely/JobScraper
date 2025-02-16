e18fd0f88051d92295101e6c2c76b41cb692faa4
Huszics Peter <huszicspeter@gmail.com>
2025-02-16 10:46:59 +0100

    rewrote melodiak code

--- JobScraper.py
+++ JobScraper.py
@@ -7,8 +7,7 @@ from slugify import slugify
 
 # FurgeDiak
 
-# URL of the API endpoint
-job_url = "https://www.furgediak.hu/furgediak-backings/methods/jobAdvertisementControl/findJobAdvertisementByParameter"
+fg_url = "https://www.furgediak.hu/furgediak-backings/methods/jobAdvertisementControl/findJobAdvertisementByParameter"
 settlement_url = "https://www.furgediak.hu/furgediak-backings/methods/employeeUserControl/getComboData"
 
 settlement_payload = {
@@ -19,9 +18,8 @@ settlement_payload = {
     }
 }
 
-# Headers (adjust as needed)
 headers = {
-    "Referer": "https://www.furgediak.hu/diakmunka"  # Include the originating page for context
+    "Referer": "https://www.furgediak.hu/diakmunka"
 }
 
 settlement_response = requests.post(settlement_url, headers=headers, json=settlement_payload)
@@ -31,38 +29,34 @@ if settlement_response.status_code == 200:
     settlement_data = settlement_response.json()
     
     def get_city_id(settlement_name, json_data):
-        for settlement in json_data.get("settlement", []):  # Assuming "settlement" key contains the data
+        for settlement in json_data.get("settlement", []):
             slug_city = slugify(settlement.get("name", ""))
             if slug_city == slugify(settlement_name):
                 return {
                     "id": settlement.get("id"),
                     "city": settlement.get("name")
-                }  # Return the ID if the city name matches
-        return None  # Return None if no match is found
-
+                }
+        return None
     user_city = input("Enter the city name: ")
     settlement_return = get_city_id(user_city, settlement_data)
 
-# Data payload
 job_payload = {
-    "countyId": None,  # County ID
-    "jobAreaId": None,  # Job area IDs as a list
-    "keys": None,  # Null in JSON is represented as None in Python
-    "newJobAdvertisement": False,  # Boolean value
-    "settlementId": settlement_return["id"],  # Settlement ID
-    "staticJobAreaId": None,  # Null value
-    "younger18": False  # Boolean value
+    "countyId": None,
+    "jobAreaId": None,
+    "keys": None,  
+    "newJobAdvertisement": False,  
+    "settlementId": settlement_return["id"],  
+    "staticJobAreaId": None,  
+    "younger18": False
 }
 
-# Send the POST request
-job_response = requests.post(job_url, headers=headers, json=job_payload)
+fg_response = requests.post(fg_url, headers=headers, json=job_payload)
 
 job_listings = []
 
-# Print the response
-if job_response.status_code == 200:
+if fg_response.status_code == 200:
     print("Success!")
-    job_data = job_response.json()
+    job_data = fg_response.json()
     for job in job_data:
         title = job.get("positionName", "N/A")
         city = job.get("settlementName", "N/A")
@@ -72,10 +66,9 @@ if job_response.status_code == 200:
             "City": city,
             "Payment": payment,})
 
-    print(job_listings)  # Assuming the server responds with JSON
+    print(job_listings)
 else:
-    print(f"Failed with status code {job_response.status_code}")
-    print(job_response.text)  # Print error messages for debugging
+    print(f"Failed with status code {fg_response.status_code}")
 
 print("-----------------------------------------------")
 # PannonWork
@@ -85,7 +78,6 @@ pw_url = "https://szovetkezet.pannonwork.hu/-/items/toborzas"
 apply_filters = True  
 under_18 = True
 summer_job = True
-
 
 pw_payload = {
     "statusz": {"_eq": "aktiv"},
@@ -113,11 +105,7 @@ if apply_filters:
         "cimkek": {"cimke_id": {"_in": filter_ids}}
     })
 
-pw_headers = {
-    "Content-Type": "application/json"
-}
-
-pw_response = requests.get(pw_url, headers=headers, params={"filter": json.dumps(pw_payload)})
+pw_response = requests.get(pw_url, params={"filter": json.dumps(pw_payload)})
 pw_job_listings = []
 
 if pw_response.status_code == 200:
@@ -135,57 +123,50 @@ if pw_response.status_code == 200:
 else:
     print(f"Request failed with status code: {pw_response.status_code}")
 
-
+print("-----------------------------------------------")
 # MeloDiak
-'''
-labels = ["18-alatt-is-vegezheto-munkak", "hetvegi-munkak", "delutanos-munkak", "ejszakas-munkak"]
-def_url = "https://web-api.melodiak.hu/v1/job-advertisement?filter%5Bregion%5D=rregion&filter%5Bcity%5D=ccity&filter%5Blabel%5D=llabel&filter%5Bpage=1&sort=-recent&_=1734547653732"
+md_url = "https://web-api.melodiak.hu/v1/job-advertisement"
 
-def on_submit_actions(user_inputs, job_types):
-    
-    # Slugify the region and city from user_inputs
-    job_region_slug = slugify(user_inputs["Region"])
-    job_city_slug = slugify(user_inputs["City"])
-    
-    # Replace the placeholders in def_url with the slugified values
-    mod_def_url = def_url.replace("rregion", job_region_slug)
-    mod_def_url = mod_def_url.replace("ccity", job_city_slug)
-    
-    # Create the job type query string from selected job_types
-    job_type_query = "%7C".join(filter(None, job_types))
-    
-    if job_types == ["", "", "", ""]:
-        mod_def_url = re.sub(r"filter%5Blabel%5D=[^&]*&", "", mod_def_url)
-    else:
-        mod_def_url = mod_def_url.replace("llabel", job_type_query)
-    print(mod_def_url)
-    # Make the GET request to the API
-    response = requests.get(mod_def_url)
-    formatted_job_listings = []
-    
-    if response.status_code == 200:
-        data = response.json()
-        job_listings = data.get("data", {}).get("resource", [])
-    
-        for job in job_listings:
-            title = job.get("title", "N/A")
-            city = job.get("city_name", "N/A")
-            payment = job.get("payment", "N/A")
-            formatted_job_listings.append({
-                "Job Title": title,
-                "City": city,
-                "Payment": payment
-            })
-    else:
-        print(f"Failed to fetch data. Status code: {response.status_code}")
-    
-    return formatted_job_listings
+md_payload = {
+    "page": 1,
+    "sort": "-recent",
+    "_": "1739657308023"
+}
 
-# Sample user inputs 
-user_inputs = {"Region": "", "City": "Budapest"}
-# Simulate selected job types.
-job_types = ["18-alatt-is-vegezheto-munkak", "hetvegi-munkak", "", ""]
+def add_filter(payload, city=None, label=None):
+    if city:
+        payload["filter[city]"] = city
+    if label:
+        payload["filter[label]"] = label
+    return payload
 
-results = on_submit_actions(user_inputs, job_types)
-print(results)
-'''
+city_filter = "budapest"  # Set to None if no city filter is needed
+label_filter = "nyari-munkak|18-alatt-is-vegezheto-munkak"  # Set to None if no label filter is needed
+
+payload = add_filter(md_payload, city=city_filter, label=label_filter)
+
+response = requests.get(md_url, params=payload)
+
+md_job_listings = []
+    
+if response.status_code == 200:
+    data = response.json()
+    job_listings = data.get("data", {}).get("resource", [])
+
+    for job in job_listings:
+        title = job.get("title", "N/A")
+        city = job.get("city_name", "N/A")
+        payment = job.get("payment", "N/A")
+        md_job_listings.append({
+            "Job Title": title,
+            "City": city,
+            "Payment": payment
+        })
+else:
+    print(f"Failed to fetch data. Status code: {response.status_code}")
+
+print(md_job_listings)
+
+print("-----------------------------------------------")
+# ProDiak
+
